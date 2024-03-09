108467d3f1a623977c5a8b79b46f4dc859dbbd2f
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2024-03-09 12:25:35 +0530

    live file changes

--- resources/views/admin/orders/partials/addGlassProductDetails.blade.php
+++ resources/views/admin/orders/partials/addGlassProductDetails.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 
-    <div class="col-xs-8 col-lg-5 col-md-12 col-sm-12">
+    <div class="col-xs-12 col-lg-5 col-md-5 col-sm-5">
         <div class="quantity-content"> 
             @if(isset($product) && ($product->is_height || $product->is_width || $product->is_length))
             <input type="hidden" name="product_category_id" class="product_category_id" value="{{$product['calculation_type'] ?? '' }}" />
@@ -34,14 +34,14 @@
         </div>
     </div>
 
-    <div class="col-xs-3 col-lg-4 col-md-3  col-sm-12">
+    <div class="col-xs-12 col-lg-4 col-md-4 col-sm-4">
         <div class="form-group">
             <label>@lang('admin_master.g_number_of_item')</label>
             <input type="number" value="0" class="form-control glass-input glass-no-item" min="0" max="999999" onkeydown="javascript: return ['Backspace','Delete','ArrowLeft','ArrowRight','Tab','Period','NumpadDecimal'].includes(event.code) ? true : !isNaN(Number(event.key)) && event.code!=='Space' && this.value.length <= 6" required />
         </div>
     </div>
 
-    <div class="col-xs-1 col-lg-3 col-md-1  col-sm-1 mt-10" style="margin-top: 25px;">
+    <div class="col-xs-12 col-lg-3 col-md-3 col-sm-3 mt-10 delete_row_block">
         <div class="form-group">
             <button type="button" class="deleteGlassRow pull-right btn btn-danger">
                 <i class="fa fa-minus"></i>
--- resources/views/admin/orders/partials/editGlassProductDetails.blade.php
+++ resources/views/admin/orders/partials/editGlassProductDetails.blade.php
@@ -3,7 +3,7 @@
 @foreach($otherDetails as $orderProduct)
 <div class="row">
 
-    <div class="col-xs-8 col-lg-5 col-md-12 col-sm-12">
+    <div class="col-xs-12 col-lg-5 col-md-5 col-sm-5">
         <div class="quantity-content">
             @if(isset($orderProduct) && ($orderProduct['height'] || $orderProduct['width'] || $orderProduct['length']))
             <input type="hidden" name="product_category_id" class="product_category_id" value="{{$product[0] ?? '' }}" />
@@ -37,14 +37,14 @@
         </div>
     </div>
 
-    <div class="col-xs-3 col-lg-4 col-md-3  col-sm-12">
+    <div class="col-xs-12 col-lg-4 col-md-4 col-sm-4">
         <div class="form-group">
             <label>@lang('admin_master.g_number_of_item')</label>
             <input type="number" value="{{ $orderProduct['qty'] }}" class="form-control glass-input glass-no-item" min="0" max="999999" onkeydown="javascript: return ['Backspace','Delete','ArrowLeft','ArrowRight','Tab','Period','NumpadDecimal'].includes(event.code) ? true : !isNaN(Number(event.key)) && event.code!=='Space' && this.value.length <= 6" required />
         </div>
     </div>
 
-    <div class="col-xs-1 col-lg-3 col-md-1  col-sm-1 mt-10" style="margin-top: 25px;">
+    <div class="col-xs-12 col-lg-3 col-md-3 col-sm-3 mt-10 delete_row_block">
         <div class="form-group">
             <button type="button" class="deleteGlassRow pull-right btn btn-danger">
                 <i class="fa fa-minus"></i>
