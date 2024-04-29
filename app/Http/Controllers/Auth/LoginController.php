48f5aaa1ceab01c54979f5e239fb566caad5915a
Khushi_soni <khushisoni.hipl@gmail.com>
2024-04-29 17:40:00 +0530

    enable ip check in login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,11 +34,11 @@ class LoginController extends Controller
             $user = User::where('username',$request->username)->first();
             if($user){
                 if (Auth::attempt($credentialsOnly, $remember_me)) {
-                //    $check_ip = checkRoleIpPermission($request->ip(), $user->roles->first()->id);
-                //    if($check_ip == "No" && $user->roles->first()->id != 1){
-                //         Auth::guard('web')->logout();
-                //         return redirect()->route('login')->withErrors(['wrongcrendials' => trans('auth.iperror')])->withInput($request->only('username', 'password'));
-                //    }
+                   $check_ip = checkRoleIpPermission($request->ip(), $user->roles->first()->id);
+                   if($check_ip == "No" && $user->roles->first()->id != 1){
+                        Auth::guard('web')->logout();
+                        return redirect()->route('login')->withErrors(['wrongcrendials' => trans('auth.iperror')])->withInput($request->only('username', 'password'));
+                   }
 
                     addToLog($request,'User','Login successfully');
                     return redirect()->route('admin.orders.create')->with(['success' => true,
