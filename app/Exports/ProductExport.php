7f60cb3cb4c903e848563e12d4e397914e711137
Khushi_soni <khushisoni.hipl@gmail.com>
2024-05-07 12:59:12 +0530

    Product excel export error Bug Fixed

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -19,42 +19,51 @@ class ProductExport implements FromCollection , WithHeadings
 
     public function collection()
     {
+        try{
+            $query = Product::query();
 
-        $query = Product::query();
+            if ($this->product_id !== null) {
+                $query->where('id', $this->product_id);
+            }
 
-        if ($this->product_id !== null) {
-            $query->where('id', $this->product_id);
+            $query->select(['products.*','groups.name as group_name','sub_group.name as sub_group_name','product_units.name as product_unit_name']);
+            $query->leftJoin('groups', 'groups.id', '=', 'products.group_id');
+            $query->leftJoin('groups as sub_group', 'sub_group.id', '=', 'products.sub_group_id');
+            $query->leftJoin('product_units', 'product_units.id', '=', 'products.unit_type')->orderBy('products.id','DESC');
+            $groups = $query->get();
+
+            return $groups->map(function ($group, $key) {
+                $html = "";
+                $calculation = config('constant.calculationType');
+                // $html .= $calculation[$group->calculation_type];
+                if (isset($calculation[$group->calculation_type])) {
+                    $html .= $calculation[$group->calculation_type];
+                } else {
+                    $html .= "Unknown"; // Or any default value you want to set
+                }
+                  return [
+                    'Sn.' => $key + 1,
+                    'Name' => $group->name ?? "",
+                    'Calculation Type' =>$html ?? "",
+                    'Unit Type' => $group->product_unit_name ?? "",
+                    'Group' => $group->group_name ?? "",
+                    'Sub Group' => $group->sub_group_name ?? "",
+                    'Height' => $group->is_height ?? '',
+                    'Width' => $group->is_width ?? '',
+                    'Length' => $group->is_length ?? '',
+                    'Is Sub Product' => $group->is_sub_product ?? '',
+                    'Hint' => $group->extra_option_hint ?? '',
+                    'Purchase Price' => $group->price ?? 0,
+                    'Min. Sale Price' => $group->min_sale_price ?? 0,
+                    'Wholesaler price' => $group->wholesaler_price ?? 0,
+                    'Retailer price' => $group->retailer_price ?? 0,
+                ];
+            });
+        }catch(\Exception $e){
+            dd($e->getMessage(),$e->getLine());
         }
 
-        $query->select(['products.*','groups.name as group_name','sub_group.name as sub_group_name','product_units.name as product_unit_name']);
-        $query->leftJoin('groups', 'groups.id', '=', 'products.group_id');
-        $query->leftJoin('groups as sub_group', 'sub_group.id', '=', 'products.sub_group_id');
-        $query->leftJoin('product_units', 'product_units.id', '=', 'products.unit_type')->orderBy('products.id','DESC');
-        $groups = $query->get();
 
-        return $groups->map(function ($group, $key) {
-            $html = "";
-            $calculation = config('constant.calculationType');                
-            $html .= $calculation[$group->calculation_type];
-              return [
-                'Sn.' => $key + 1,
-                'Name' => $group->name ?? "",
-                'Calculation Type' =>$html ?? "",
-                'Unit Type' => $group->product_unit_name ?? "",
-                'Group' => $group->group_name ?? "",
-                'Sub Group' => $group->sub_group_name ?? "",
-                'Height' => $group->is_height ?? '',
-                'Width' => $group->is_width ?? '',
-                'Length' => $group->is_length ?? '',
-                'Is Sub Product' => $group->is_sub_product ?? '',
-                'Hint' => $group->extra_option_hint ?? '',
-                'Purchase Price' => $group->price ?? 0,
-                'Min. Sale Price' => $group->min_sale_price??0,
-                'Wholesaler price' => $group->wholesaler_price??0,
-                'Retailer price' => $group->retailer_price??0,
-            ];
-        });
-       
     }
 
     public function headings(): array
