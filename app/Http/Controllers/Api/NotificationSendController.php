55470a5c4599bb32d0a8c1addb650e8d5b096e14
Khushi_soni <khushisoni.hipl@gmail.com>
2024-05-10 16:16:33 +0530

    bug fix in api

--- app/Http/Controllers/Api/NotificationSendController.php
+++ app/Http/Controllers/Api/NotificationSendController.php
@@ -7,6 +7,7 @@ use App\Models\Notification;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NotificationSendController extends Controller
 {
